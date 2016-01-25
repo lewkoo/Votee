b95f421c1a7d0c1678a795bdd4dfc60d9f6faec6
lewkoo <lewkoo@gmail.com>
2016-01-25 13:25:03 -0600

    Correct role assignment during the registration

--- packages/core/users/public/services/meanUser.js
+++ packages/core/users/public/services/meanUser.js
@@ -137,7 +137,8 @@ angular.module('mean.users').factory('MeanUser', [ '$rootScope', '$http', '$loca
         password: user.password,
         confirmPassword: user.confirmPassword,
         username: user.username,
-        name: user.name
+        name: user.name,
+        roles: user.roles
       })
         .success(this.onIdentity.bind(this))
         .error(this.onIdFail.bind(this));
