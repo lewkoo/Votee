383c05f5ea178765c623143241a02906e386c6f9
norkie <yuriy.ace@gmail.com>
2016-02-04 13:55:27 -0600

    Added back end logic for questions

---
+++ packages/custom/questions/server/controllers/questions.js
@@ -0,0 +1,139 @@
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Article = mongoose.model('Article'),
+    config = require('meanio').loadConfig(),
+    _ = require('lodash');
+
+module.exports = function(Questions) {
+
+    return {
+        /**
+         * Find question by id
+         */
+        question: function(req, res, next, id) {
+            Article.load(id, function(err, article) {
+                if (err) return next(err);
+                if (!article) return next(new Error('Failed to load article ' + id));
+                req.article = article;
+                next();
+            });
+        },
+        /**
+         * Create a question
+         */
+        create: function(req, res) {
+            var question = new Questions(req.body);
+            question.user = req.user;
+
+            question.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        error: 'Cannot save the article'
+                    });
+                }
+
+                Questions.events.publish({
+                    action: 'created',
+                    user: {
+                        name: req.user.name
+                    },
+                    url: config.hostname + '/articles/' + question._id,
+                    name: question.title
+                });
+
+                res.json(question);
+            });
+        },
+        /**
+         * Update an article
+         */
+        update: function(req, res) {
+            var article = req.article;
+
+            article = _.extend(article, req.body);
+
+
+            article.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        error: 'Cannot update the article'
+                    });
+                }
+
+                Articles.events.publish({
+                    action: 'updated',
+                    user: {
+                        name: req.user.name
+                    },
+                    name: article.title,
+                    url: config.hostname + '/articles/' + article._id
+                });
+
+                res.json(article);
+            });
+        },
+        /**
+         * Delete an article
+         */
+        destroy: function(req, res) {
+            var article = req.article;
+
+
+            article.remove(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        error: 'Cannot delete the article'
+                    });
+                }
+
+                Articles.events.publish({
+                    action: 'deleted',
+                    user: {
+                        name: req.user.name
+                    },
+                    name: article.title
+                });
+
+                res.json(article);
+            });
+        },
+        /**
+         * Show a question
+         */
+        show: function(req, res) {
+
+            Questions.events.publish({
+                action: 'viewed',
+                user: {
+                    name: req.user.name
+                },
+                name: req.article.title,
+                url: config.hostname + '/articles/' + req.article._id
+            });
+
+            res.json(req.article);
+        },
+        /**
+         * List of Question
+         */
+        all: function(req, res) {
+            var query = req.acl.query('Questions');
+
+            query.find({}).sort('-created').populate('user', 'name username').exec(function(err, questions) {
+                if (err) {
+                    return res.status(500).json({
+                        error: 'Cannot list the questions'
+                    });
+                }
+
+                res.json(questions)
+            });
+
+        }
+    };
+}
--- packages/custom/questions/server/routes/questions.js
+++ packages/custom/questions/server/routes/questions.js
@@ -1,27 +1,65 @@
 'use strict';
 
+
+// Question authorization helper for profs
+var hasAuthorization = function(req, res, next) {
+    if (!req.question.user._id.equals(req.user._id)) {
+        return res.status(401).send('User is not authorized');
+    }
+    next();
+};
+
+var hasPermissions = function(req, res, next) {
+
+    req.body.permissions = req.body.permissions || ['authenticated'];
+
+    for (var i = 0; i < req.body.permissions.length; i++) {
+        var permission = req.body.permissions[i];
+        if (req.acl.user.allowed.indexOf(permission) === -1) {
+            return res.status(401).send('User not allowed to assign ' + permission + ' permission.');
+        }
+    }
+
+    next();
+};
+
 /* jshint -W098 */
 // The Package is past automatically as first parameter
-module.exports = function(Questions, app, auth, database) {
+module.exports = function(Questions, app, auth) {
 
-  app.get('/api/questions/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
+var questions = require('../controllers/questions')(Questions);
 
-  app.get('/api/questions/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
 
-  app.get('/api/questions/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
+    //app.get('/api/questions/example/anyone', function(req, res, next) {
+  //  res.send('Anyone can access this');
+  //});
+  //
+  //app.get('/api/questions/example/auth', auth.requiresLogin, function(req, res, next) {
+  //  res.send('Only authenticated users can access this');
+  //});
+  //
+  //app.get('/api/questions/example/admin', auth.requiresAdmin, function(req, res, next) {
+  //  res.send('Only users with Admin role can access this');
+  //});
+  //
+  //app.get('/api/questions/example/render', function(req, res, next) {
+  //  Questions.render('index', {
+  //    package: 'questions'
+  //  }, function(err, html) {
+  //    //Rendering a view from the Package server/views
+  //    res.send(html);
+  //  });
+  //});
 
-  app.get('/api/questions/example/render', function(req, res, next) {
-    Questions.render('index', {
-      package: 'questions'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
-    });
-  });
+
+    app.route('/api/questions')
+        .get(questions.all, auth.requiresLogin)
+        .post(auth.requiresLogin, hasPermissions, questions.create);
+    app.route('/api/questions/:questionsId')
+        .get(auth.isMongoId, auth.requiresLogin, questions.show)
+        .put(auth.isMongoId, auth.requiresLogin, auth.requiresProf, hasAuthorization, hasPermissions, questions.update)
+        .delete(auth.isMongoId, auth.requiresLogin, auth.requiresProf, hasAuthorization, hasPermissions, questions.destroy);
+
+// Finish with setting up the articleId param
+    app.param('questionId', questions.question);
 };
