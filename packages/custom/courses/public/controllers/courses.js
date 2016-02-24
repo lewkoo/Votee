4d33a3aeefba7c301751f263746aea063281e599
lewkoo <lewkoo@gmail.com>
2016-02-24 17:50:33 -0600

    New gulp tasks + trying to fix front end testing
    Signed-off-by: lewkoo <lewkoo@gmail.com>

--- packages/custom/courses/public/controllers/courses.js
+++ packages/custom/courses/public/controllers/courses.js
@@ -1,11 +1,18 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.courses').controller('CoursesController', ['$scope', 'Global', 'Courses',
-  function($scope, Global, Courses) {
-    $scope.global = Global;
-    $scope.package = {
-      name: 'courses'
-    };
-  }
+angular.module('mean.courses').controller('CoursesController', ['$scope', '$stateParams', 'Global', 'Courses', '$location', 'MeanUser', 'Circles', '$http',
+    function($scope, $stateParams, Global, Courses, $location, MeanUser, Circles, $http) {
+        $scope.global = Global;
+        $scope.package = {
+            name: 'courses'
+        };
+
+        $scope.find = function() {
+            Courses.query(function(questions) {
+                $scope.questions = questions;
+            });
+        };
+
+    }
 ]);
--- packages/custom/courses/public/tests/courses.spec.js
+++ packages/custom/courses/public/tests/courses.spec.js
@@ -5,9 +5,105 @@
 
 (function() {
     describe('Courses front end controller', function () {
-        it('1 should be equals to 1', function () {
-            expect(1).toBe(1);
+
+        beforeEach(function() {
+            jasmine.addMatchers({
+                toEqualData: function() {
+                    return {
+                        compare: function(actual, expected) {
+                            return {
+                                pass: angular.equals(actual, expected)
+                            };
+                        }
+                    };
+                }
+            });
         });
+
+        beforeEach(function() {
+            module('mean');
+            module('mean.system');
+            module('mean.courses');
+        });
+
+        // Initialize the controller and a mock scope
+        var CoursesController,
+            scope,
+            $httpBackend,
+            $stateParams,
+            $location;
+
+        // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+        // This allows us to inject a service but then attach it to a variable
+        // with the same name as the service.
+        beforeEach(inject(function($controller, $rootScope, $injector, _$location_, _$stateParams_, _$httpBackend_) {
+
+            scope = $rootScope.$new();
+
+            CoursesController = $controller('CoursesController', {
+                $scope: scope
+            });
+
+            $stateParams = _$stateParams_;
+
+            $httpBackend = $injector.get("$httpBackend");
+
+            $location = _$location_;
+
+        }));
+
+        var testCourseData = function() {
+          return {
+              "_id" : "56c8bdfaf82d7bd71d40de08",
+              "title" : "Test Course 1",
+              "courseNumber" : 4350,
+              "description" : "This is a test 1",
+              "professor" : "56c8bdfaf82d7bd71d40de02",
+              "questions" : [],
+              "students" : [
+                  "56c8bdfaf82d7bd71d40de03",
+                  "56c8bdfaf82d7bd71d40de04",
+                  "56c8bdfaf82d7bd71d40de05",
+                  "56c8bdfaf82d7bd71d40de06",
+                  "56c8bdfaf82d7bd71d40de07"
+              ],
+              "created" : "2016-02-20T19:26:50.874Z",
+              "__v" : 0
+          };
+        };
+
+        it('$scope.find() should find courses ', function() {
+
+
+            console.log($httpBackend);
+
+            // mock the expected responce to a GET request
+            $httpBackend.expectGET(/api\/courses$/).respond([{
+                "_id" : "56c8bdfaf82d7bd71d40de08",
+                "title" : "Test Course 1",
+                "courseNumber" : 4350,
+                "description" : "This is a test 1",
+                "professor" : "56c8bdfaf82d7bd71d40de02",
+                "questions" : [],
+                "students" : [
+                    "56c8bdfaf82d7bd71d40de03",
+                    "56c8bdfaf82d7bd71d40de04",
+                    "56c8bdfaf82d7bd71d40de05",
+                    "56c8bdfaf82d7bd71d40de06",
+                    "56c8bdfaf82d7bd71d40de07"
+                ],
+                "created" : "2016-02-20T19:26:50.874Z",
+                "__v" : 0
+            }]);
+
+            scope.find();
+            $httpBackend.flush();
+
+            // test the return value
+            expect(scope.courses).toEqualData(testCourseData());
+
+        });
+
     });
 
 
