82704534da3256abbaf16e10ca9c5b9f1da7496a
norkie <yuriy.ace@gmail.com>
2016-02-24 08:09:49 -0600

    Front end questions test stub

---
+++ packages/custom/questions/public/tests/questions.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yuriy on 2016-02-24.
+ */
+'use strict';
+
+(function() {
+    describe('Questions front end controller', function () {
+        it('1 should be equals to 1', function () {
+            expect(1).toBe(1);
+        });
+    });
+
+
+}());
