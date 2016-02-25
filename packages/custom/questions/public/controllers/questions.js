cfcbbce9b585d3829e69517dc73ac34d4f4a0467
sandersjessica <sandersjessicamarie@gmail.com>
2016-02-25 14:17:55 -0600

    test

--- packages/custom/questions/public/controllers/questions.js
+++ packages/custom/questions/public/controllers/questions.js
@@ -1,16 +1,46 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.questions').controller('QuestionsController', ['$scope', '$stateParams', 'Global', 'Questions', '$location', 'MeanUser', 'Circles',
-    function($scope,$stateParams, Global, Questions, $location, MeanUser, Circles) {
+angular.module('mean.questions').controller('QuestionsController', ['$scope', '$stateParams', 'Global', 'Questions', '$location', 'MeanUser', 'Circles', '$http',
+    function($scope,$stateParams, Global, Questions, $location, MeanUser, Circles, $http) {
         $scope.global = Global;
         $scope.package = {
             name: 'questions'
         };
 
-        $scope.isAuthorized = MeanUser.isProfessor;
+        $scope.hasAuthorization = function(question) {
+            return MeanUser.isProfessor;
+        };
+
+        //$scope.isAuthorized = MeanUser.isProfessor;
+
+        $scope.hasVoted = function(question){
+
+           /*
+            for(var key in question.answers){
+                var answer = question.answers[key];
+
+                console.log(answer);
+                var creator = answer.student;
+                console.log(creator);
+                if(creator.equals(MeanUser._id)){
+                        return true;
+                }
+            }*/
+            return false;
+        };
 
         $scope.availableCircles = [];
+        //store options text in the array
+        $scope.optionsText = [];
+        $scope.selectedAnswer = { index: 0 };
+
+        //$scope.setSelected = function(selected) {
+        //    console.log(selected);
+        //    $scope.selected = selected;
+        //}
+
+        //$scope.selectedAnswer ;
 
         Circles.mine(function(acl) {
             $scope.availableCircles = acl.allowed;
@@ -29,8 +59,23 @@ angular.module('mean.questions').controller('QuestionsController', ['$scope', '$
 
             } else {
                 $scope.submitted = true;
-
             }
+        };
+
+        $scope.vote = function(question) {
+            console.log("Submitting vote");
+            var question = $scope.question;
+            if (!question.updated) {
+                question.updated = [];
+            }
+            question.updated.push(new Date().getTime());
+
+            question.$vote(function() {
+                $location.path('questions/vote/' + question._id);
+            });
+
+            console.log("END of function");
+
         };
 
         $scope.find = function() {
@@ -44,8 +89,47 @@ angular.module('mean.questions').controller('QuestionsController', ['$scope', '$
                 questionId: $stateParams.questionId
             }, function(question) {
                 $scope.question = question;
+                //console.log($scope.question.options[0]);
             });
         };
+
+        $scope.update = function(isValid) {
+            if (isValid) {
+                var question = $scope.question;
+                if (!question.updated) {
+                    question.updated = [];
+                }
+                question.updated.push(new Date().getTime());
+
+                question.$update(function() {
+                    $location.path('questions/' + question._id);
+                });
+            } else {
+                $scope.submitted = true;
+            }
+        };
+
+        $scope.remove = function(question) {
+            console.log(question);
+            if (question) {
+                question.$remove(function(response) {
+                    for (var i in $scope.questions) {
+                        if ($scope.questions[i] === question) {
+                            $scope.questions.splice(i, 1);
+                        }
+                    }
+                    $location.path('questions');
+                });
+            } else {
+                $scope.question.$remove(function(response) {
+                    $location.path('questions');
+                });
+            }
+        };
+
+        $scope.submitAnswer = function(form){
+            console.log(form)
+        }
 
     }
 ]);
