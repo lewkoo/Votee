61554cada976e6a78355b3fc5ef25598433f52cf
norkie <yuriy.ace@gmail.com>
2016-02-25 14:25:33 -0600

    First front end testing for questions. Added a package needed for phantomJS

--- packages/custom/questions/public/tests/questions.spec.js
+++ packages/custom/questions/public/tests/questions.spec.js
@@ -5,10 +5,106 @@
 
 (function() {
     describe('Questions front end controller', function () {
-        it('1 should be equals to 1', function () {
-            expect(1).toBe(1);
-        });
-    });
+        //it('1 should be equals to 1', function () {
+        //    expect(1).toBe(1);
+        //});
 
+        beforeEach(function() {
+            jasmine.addMatchers({
+                toEqualData: function() {
+                    return {
+                        compare: function(actual, expected) {
+                            return {
+                                pass: angular.equals(actual, expected)
+                            };
+                        }
+                    };
+                }
+            });
+        });
+
+        beforeEach(function() {
+            module('mean');
+            module('mean.system');
+            module('mean.questions');
+            module('mean.courses');
+        });
+
+        // Initialize the controller and a mock scope
+        var QuestionsController,
+            scope,
+            $httpBackend,
+            $stateParams,
+            $location;
+
+        // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+        // This allows us to inject a service but then attach it to a variable
+        // with the same name as the service.
+        beforeEach(inject(function($controller, $rootScope, $injector, _$location_, _$stateParams_, _$httpBackend_) {
+
+            scope = $rootScope.$new();
+
+            QuestionsController = $controller('QuestionsController', {
+                $scope: scope
+            });
+
+            $stateParams = _$stateParams_;
+
+            $httpBackend = _$httpBackend_;
+
+            $location = _$location_;
+
+            // mock these requests
+            $httpBackend.expectGET('/api\/users\/me').respond(200);
+            $httpBackend.when('GET','voteeHome\/views\/index.html').respond(200);
+            $httpBackend.when('GET','/api\/circles\/mine').respond(200);
+
+        }));
+
+        var testQuetionsData = function() {
+            return {
+                "_id": "56cf5578b387fd7c940cb9be",
+                "creator": "56b2a9b3897e13640eeba6e9",
+                "title": "New q",
+                "options": {"0": "sdsdj", "1": "kkk", "2": "nnn", "3": "dfd"},
+                "answer": "2",
+                "answers": [
+                    "56cf557fb387fd7c940cb9bf"
+                ],
+                "type": "MULTIPLE-CHOICE",
+                "created": "2016-02-25T19:26:48.686Z",
+                "__v": 1
+            };
+        }; //testQuestionsData
+
+        it('$scope.find() should find questions ', function() {
+
+            // mock the expected response to a GET request
+
+            $httpBackend.expectGET(/api\/questions$/).respond([{
+                "_id": "56cf5578b387fd7c940cb9be",
+                "creator": "56b2a9b3897e13640eeba6e9",
+                "title": "New q",
+                "options": {"0": "sdsdj", "1": "kkk", "2": "nnn", "3": "dfd"},
+                "answer": "2",
+                "answers": [
+                    "56cf557fb387fd7c940cb9bf"
+                ],
+                "type": "MULTIPLE-CHOICE",
+                "created": "2016-02-25T19:26:48.686Z",
+                "__v": 1
+            }]);
+
+            scope.find();
+            $httpBackend.flush();
+
+            // test the return value
+            expect(scope.questions[0]).toEqualData(testQuetionsData());
+
+        });
+
+
+
+    });
 
 }());
