80504864bdfb056e2ca7e6398fc952a1e52fa3b6
lewkoo <lewkoo@gmail.com>
2016-02-25 15:01:53 -0600

    Front end testing for create function
    Signed-off-by: lewkoo <lewkoo@gmail.com>

--- packages/custom/courses/public/controllers/courses.js
+++ packages/custom/courses/public/controllers/courses.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.courses').controller('CoursesController', ['$scope', '$stateParams', 'Global', 'Courses', '$location', 'MeanUser', 'Circles', '$http',
-    function($scope, $stateParams, Global, Courses, $location, MeanUser, Circles, $http) {
+angular.module('mean.courses').controller('CoursesController', ['$scope', '$stateParams', 'Global', 'Courses', '$location', 'MeanUser',
+    function($scope, $stateParams, Global, Courses, $location, MeanUser) {
         $scope.global = Global;
         $scope.package = {
             name: 'courses'
@@ -13,6 +13,49 @@ angular.module('mean.courses').controller('CoursesController', ['$scope', '$stat
                 $scope.courses = courses;
             });
         };
+
+        $scope.create = function(isValid) {
+            if(isValid) {
+
+                car course = new Courses($scope.course);
+                /*var course = new Courses({
+                    title: $scope.title,
+                    courseNumber: $scope.courseNumber,
+                    description: $scope.description,
+                    professor: $scope.professor,
+                    questions: $scope.questions,
+                    students: $scope.students,
+                    created: $scope.created,
+                    __v: $scope.__v
+                });*/
+
+                course.$save(function(response){
+                   $location.path('courses/' + response._id);
+                });
+
+                $scope.title = '';
+                $scope.courseNumber = 0;
+                $scope.description = '';
+                $scope.professor = '';
+                $scope.questions = [];
+                $scope.students = [];
+                $scope.created = '';
+                $scope.__v = 0;
+
+            } else {
+                $scope.submitted = true;
+            }
+        };
+
+        $scope.findOne = function() {
+            Courses.get({
+                courseId: $stateParams.courseId
+            }, function(course) {
+                $scope.course = course;
+            });
+        };
+
+
 
     }
 ]);
