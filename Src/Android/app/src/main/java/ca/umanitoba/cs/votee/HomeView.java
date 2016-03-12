e47a21f0ef9a37fffa3a35b28f5491a833b24015
norkie <yuriy.ace@gmail.com>
2016-03-11 19:12:34 -0600

    Added HomeView activity.

---
+++ Src/Android/app/src/main/java/ca/umanitoba/cs/votee/HomeView.java
@@ -0,0 +1,13 @@
+package ca.umanitoba.cs.votee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_view);
+    }
+}
--- Src/Android/app/src/main/java/ca/umanitoba/cs/votee/LoginActivity.java
+++ Src/Android/app/src/main/java/ca/umanitoba/cs/votee/LoginActivity.java
@@ -3,6 +3,7 @@ package ca.umanitoba.cs.votee;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -190,6 +191,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            //TODO: add some error checking
+            Intent intent = new Intent(this, HomeView.class);
+            startActivity(intent);
+
         }
     }
 
