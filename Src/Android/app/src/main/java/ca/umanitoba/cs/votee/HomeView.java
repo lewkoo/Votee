43ec3ffd4553610fc5f86d2c0f7ab9c8dda92d36
Nathan Loewen <loewen.nathan@gmail.com>
2016-03-15 12:38:22 -0500

    Add quiz and course fragments

--- Src/Android/app/src/main/java/ca/umanitoba/cs/votee/HomeView.java
+++ Src/Android/app/src/main/java/ca/umanitoba/cs/votee/HomeView.java
@@ -1,13 +1,23 @@
 package ca.umanitoba.cs.votee;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeView extends AppCompatActivity {
+import ca.umanitoba.cs.votee.fragments.CourseList;
+import ca.umanitoba.cs.votee.fragments.QuizList;
+
+public class HomeView extends FragmentActivity implements CourseList.OnFragmentInteractionListener, QuizList.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_view);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
