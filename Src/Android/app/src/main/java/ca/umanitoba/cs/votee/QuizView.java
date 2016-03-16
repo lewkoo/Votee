916282d75bbbc260f2a9aa77f69b8932c49a92cb
Yuriy <yuriy.ace@gmail.com>
2016-03-16 18:41:04 -0500

    Fixed up the built error.

--- Src/Android/app/src/main/java/ca/umanitoba/cs/votee/QuizView.java
+++ Src/Android/app/src/main/java/ca/umanitoba/cs/votee/QuizView.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
