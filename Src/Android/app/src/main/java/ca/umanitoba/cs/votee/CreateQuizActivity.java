fa1dafa08d727379879dc355ff9eb2f23e619cde
Yuriy <yuriy.ace@gmail.com>
2016-03-17 18:55:20 -0500

    Typo fix

--- Src/Android/app/src/main/java/ca/umanitoba/cs/votee/CreateQuizActivity.java
+++ Src/Android/app/src/main/java/ca/umanitoba/cs/votee/CreateQuizActivity.java
@@ -35,7 +35,7 @@ public class CreateQuizActivity extends BaseActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {;
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
 
